242fe23df0423ff49417d7f072293a5e6ee1a3a1
kylebeadl <kylebeadl@users.noreply.github.com>
2015-12-11 22:44:38 -0600

    E-commerce site for a fashion startup

---
+++ index.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+  <title>Lifestlye Ties</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <meta name="viewport" content="width=device-width; initial-scale=1.0">
+</head>
+<body>
+  <br>
+<img src="logo.png" id="logo"/>
+<br>
+<div id="countdown">
+<script language="JavaScript" id="text">
+TargetDate = "12/31/2016 5:00 AM";
+BackColor = "#fafcfc";
+ForeColor = "black";
+CountActive = true;
+CountStepper = -1;
+LeadingZero = true;
+DisplayFormat = "%%D%% Days, %%H%% Hours, %%M%% Minutes, %%S%% Seconds";
+FinishMessage = "It is finally here!";
+</script>
+<script language="JavaScript" src="countdown.js">
+</script>
+<br>until launch.
+</div>
+<p id="text">Lifestyle Ties is a luxury tie company.<br>We are excited to get this off the
+   ground,<br>so join our email list and we will keep you<br>updated on our
+   progress.</p>
+   <!-- Begin MailChimp Signup Form -->
+   <div id="mc_embed_signup">
+   <form action="" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+       <div id="mc_embed_signup_scroll">
+   <div class="mc-field-group">
+   	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email"><br>
+   </div>
+   <div class="mc-field-group">
+   	<input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="First Name"><br>
+   </div>
+   <div class="mc-field-group">
+   	<input type="text" value="" name="LNAME" class="" id="mce-LNAME" placeholder="Last Name"><br>
+   </div>
+   	<div id="mce-responses" class="clear">
+   		<div class="response" id="mce-error-response" style="display:none"></div>
+   		<div class="response" id="mce-success-response" style="display:none"></div>
+   	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+       <div style="position: absolute; left: -5000px;"><input type="text" name="b_b0a8983d2e687a01b92a4af33_6e7de3b86a" tabindex="-1" value=""></div>
+       <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+       </div>
+   </form>
+   </div>
+
+   <!--End mc_embed_signup-->
+<br>
+<div class="container">
+
+    <div class="fake_img"><a href="https://www.facebook.com/lifestyleties"><img src="facebook.png"/></a></div>
+    <div class="fake_img"><a href="https://twitter.com/lifestyleties"><img src="twitter.png"/></a></div>
+    <div class="fake_img"><a href="https://instagram.com/lifestyleties/"><img src="instagram.png"/></a></div>
+    <div class="fake_img"><a href="https://www.pinterest.com/lifestyleties/"><img src="pinterest.png"/></a></div>
+</div>
+</body>
+</html>
